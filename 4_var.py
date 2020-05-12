aad2bd749b831252c6367ee4a2241750c68aa836
Evgeny Yakovlev <git@b14esh.com>
2020-05-12 11:29:58 +0300

    add file 4_var.py

---
+++ 4_var.py
@@ -0,0 +1,25 @@
+a = "Hello"
+b = 25
+
+print(a)
+print(b)
+
+f_name = "Vasiliy"
+l_name = "Pupkin"
+age = 25
+full_name = (f_name + " " + l_name)
+
+print(f_name + " " + l_name)
+
+print(full_name + " " + str(age))
+
+f_name = "Vas"
+F_NAME = "VAS"
+
+print(f_name + " \= " + F_NAME)
+
+num1 = 1111111111111111
+num2 = 2222222222222222
+num3 = num1 + num2
+print(num1+num2)
+print("num3" + " == " + str(num3))
