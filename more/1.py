4984a6962e0fef47a1da82cd3b09c60d6767bf04
Evgeny Yakovlev <git@b14esh.com>
2020-07-22 11:01:53 +0300

    modifi files

---
+++ more/1.py
@@ -0,0 +1,13 @@
+num_1 = float(input ("Enter first num: "))
+num_2 = float(input("Enter second num: "))
+res = num_1 + num_2
+res += 5 #res = res + 5
+#res -= 5 #res = res - 5
+#res *= 5 #res = res * 5
+#res /= 5 #res = res / 5
+
+print("Result is:", res)
+
+RES = input("Enter something: ")
+RES *= 20
+print(">>>", RES, "<<<"  )
---
+++ more/2.py
@@ -0,0 +1,10 @@
+# словные операторы # 1 = True # 0 = False
+if 0:
+    print("Ничего не выведит")
+if 1:
+    print("True \\ Истина if равно 1")
+    print("All is okay!")
+    
+
+
+
