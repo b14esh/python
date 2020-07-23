6361c37b7eb71ad20b59a1716100b553574773fe
Evgeny Yakovlev <git@b14esh.com>
2020-07-23 12:20:07 +0300

    add new files

---
+++ more/5.py
@@ -0,0 +1,14 @@
+# цыклы for
+# работает со строками и списками
+pp = "\n\n#--------------------------------------------\n"
+
+for j in 'hello world':
+    print(j * 2)
+print(pp)
+
+for j in 'hello world':
+    print(j * 2, end = '')
+print(pp)
+for j in 'hello world':
+    print(j * 5, end = '')
+print(pp)
